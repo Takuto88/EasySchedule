f07506ebebb239cad17953995909e6355dde661a
Collin Alpert <collinalpert@gmail.com>
2017-06-27 21:48:16 +0200

    Added GUI

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class Main extends JFrame {
@@ -16,44 +17,53 @@ public class Main extends JFrame {
 
         JPanel topLeftPan = new JPanel(new BorderLayout());
         //{
-        JPanel 
+        JPanel buttonPan1 = new JPanel();
+            //{
         JButton addTeacher = new JButton("Neue Lehrkraft");
-        JButton addClass = new JButton("Neue Klasse");
-        //}
-
-        JPanel mainPan = new JPanel();
-        //{
-        JPanel teacherPan = new JPanel();
+        buttonPan1.add(addTeacher);
+            //}
+        JPanel panePan1 = new JPanel();
             //{
         JList<String> teacherList = new JList<>();
         JScrollPane teacherPane = new JScrollPane(teacherList);
         teacherPane.setPreferredSize(new Dimension(Utilities.panewidth, Utilities.paneheight));
-        teacherPan.add(teacherPane);
+        panePan1.add(teacherPane);
             //}
-        JPanel classPan = new JPanel();
+        //}
+        topLeftPan.add(buttonPan1, BorderLayout.NORTH);
+        topLeftPan.add(panePan1, BorderLayout.SOUTH);
+
+        JPanel topRightPan = new JPanel(new BorderLayout());
+        //{
+        JPanel buttonPan2 = new JPanel();
+            //{
+        JButton addClass = new JButton("Neue Klasse");
+        buttonPan2.add(addClass);
+            //}
+        JPanel panePan2 = new JPanel();
             //{
         JList<String> classList = new JList<>();
         JScrollPane classPane = new JScrollPane(classList);
         classPane.setPreferredSize(new Dimension(Utilities.panewidth, Utilities.paneheight));
-        classPan.add(classPane);
+        panePan2.add(classPane);
             //}
         //}
+        topRightPan.add(buttonPan2, BorderLayout.NORTH);
+        topRightPan.add(panePan2, BorderLayout.SOUTH);
+
 
         JPanel bottomPan = new JPanel();
         //{
         JButton generate = new JButton("Stundenplan generieren");
+        bottomPan.add(generate);
+        bottomPan.setBorder(new EmptyBorder(0, 0, 20, 0));
         //}
 
-        topPan.add(addTeacher, FlowLayout.LEFT);
-        topPan.add(addClass, FlowLayout.CENTER);
+        JPanel mainP = new JPanel();
+        mainP.add(topLeftPan);
+        mainP.add(topRightPan);
 
-        mainPan.add(teacherPan);
-        mainPan.add(classPan);
-
-        bottomPan.add(generate);
-
-        this.add(topPan, BorderLayout.NORTH);
-        this.add(mainPan, BorderLayout.CENTER);
+        this.add(mainP, BorderLayout.NORTH);
         this.add(bottomPan, BorderLayout.SOUTH);
 
 
