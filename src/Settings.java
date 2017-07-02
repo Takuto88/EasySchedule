428437671c1778495bb5ea59fea17d5bce75751b
Collin Alpert <collinalpert@gmail.com>
2017-07-02 20:27:41 +0200

    Expanding settings GUI (with functionality)

--- src/Settings.java
+++ src/Settings.java
@@ -1,14 +1,26 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Collections;
 
 public class Settings extends JFrame {
     public Settings() {
         JTabbedPane tabPane = new JTabbedPane();
         JPanel timeTable = Utilities.newBoxLayout();
-        for (int i = 0; i < 16; i++)
-            timeTable.add(Utilities.add(new JPanel(), new JLabel(i + ". Stunde: von "), new JComboBox<>(Utilities.hours),
-                    new JLabel(":"), new JComboBox<>(Utilities.minutes), new JLabel(" bis "), new JComboBox<>(Utilities.hours),
-                    new JLabel(":"), new JComboBox<>(Utilities.minutes)));
+        for (int i = 0; i < 16; i++) {
+            JComboBox<String> beginHours = new JComboBox<>(Utilities.hours);
+            JComboBox<String> beginMinutes = new JComboBox<>(Utilities.minutes);
+            JComboBox<String> endHours = new JComboBox<>(Utilities.hours);
+            JComboBox<String> endMinutes = new JComboBox<>(Utilities.minutes);
+            if(!Utilities.periods.isEmpty()) {
+                beginHours.setSelectedItem(Utilities.formatTime(Utilities.periods.get(i + "begin").getHour()));
+                endHours.setSelectedItem(Utilities.formatTime(Utilities.periods.get(i + "end").getHour()));
+                beginMinutes.setSelectedItem(Utilities.formatTime(Utilities.periods.get(i + "begin").getMinute()));
+                endMinutes.setSelectedItem(Utilities.formatTime(Utilities.periods.get(i + "end").getMinute()));
+            }
+            timeTable.add(Utilities.add(new JPanel(), new JLabel(i + ". Stunde: von "), beginHours,
+                    new JLabel(":"), beginMinutes, new JLabel(" bis "), endHours,
+                    new JLabel(":"), endMinutes));
+        }
         JButton save = new JButton("Speichern");
         save.addActionListener(e -> Utilities.savePeriods(timeTable));
         tabPane.addTab("Zeitplan", Utilities.add(Utilities.newBoxLayout(), timeTable,
@@ -16,7 +28,6 @@ public class Settings extends JFrame {
         tabPane.addTab("Fächer", subjectTab());
         tabPane.addTab("Wahlpflicht", Utilities.add(Utilities.newBoxLayout(), new JButton("Wahlpflicht")));
         this.add(tabPane);
-
         Utilities.setDefault(save);
         this.pack();
         this.setLocationRelativeTo(null);
@@ -24,19 +35,31 @@ public class Settings extends JFrame {
     }
 
     private JPanel subjectTab() {
-        JPanel subjectPan = new JPanel(new BorderLayout());
+        JPanel subjectPan = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
 
         JList<String> subjectList = new JList<>();
         ((DefaultListCellRenderer) subjectList.getCellRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
+        subjectList.setFont(Utilities.defaultFont);
         JScrollPane pane = new JScrollPane(subjectList);
+        pane.setPreferredSize(Utilities.panelSize);
+        Utilities.updateList(Utilities.subjects, subjectList);
 
         JPanel inputPan = new JPanel(new BorderLayout());
+        inputPan.setMaximumSize(new Dimension(150, 50));
         JTextField subjectIn = new JTextField();
         JButton add = new JButton("Hinzufügen");
         Utilities.format(subjectIn, add);
         add.addActionListener(e -> {
-            Utilities.subjects.add(subjectIn.getText());
-            subjectIn.setText("");
+            if (Utilities.isInList(subjectIn.getText(), Utilities.subjects))
+                Utilities.alert("Dieses Unterrichtsfach ist schon vorhanden.", null, null);
+            else {
+                Utilities.subjects.add(subjectIn.getText());
+                Collections.sort(Utilities.subjects);
+                Utilities.updateList(Utilities.subjects, subjectList);
+                subjectIn.setText("");
+                subjectIn.requestFocus();
+            }
         });
         inputPan.setSize(new Dimension(300, 200));
         inputPan.add(subjectIn, BorderLayout.NORTH);
@@ -48,12 +71,19 @@ public class Settings extends JFrame {
             int index = subjectList.getSelectedIndex();
             if (index != -1) {
                 Utilities.subjects.remove(index);
+                Collections.sort(Utilities.subjects);
                 Utilities.updateList(Utilities.subjects, subjectList);
             } else Utilities.alert("Bitte wähle ein Element aus", null, null);
         });
-        subjectPan.add(pane, BorderLayout.WEST);
-        subjectPan.add(inputPan, BorderLayout.EAST);
-        subjectPan.add(Utilities.add(new JPanel(), delete), BorderLayout.SOUTH);
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(5, 10, 5, 10);
+        subjectPan.add(pane, c);
+        c.gridy = 2;
+        subjectPan.add(delete, c);
+        c.gridx = 1;
+        c.gridy = 1;
+        subjectPan.add(inputPan);
         return subjectPan;
     }
 }
--- src/Utilities.java
+++ src/Utilities.java
@@ -20,7 +20,7 @@ public class Utilities implements Serializable {
     static final Font defaultFont = new Font("Roboto", Font.PLAIN, 14);
     static final String[] hours = {"06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22"};
     static final String[] minutes = {"00", "05", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55"};
-    private static HashMap<String, LocalTime> periods = new HashMap<>();
+    static HashMap<String, LocalTime> periods = new HashMap<>();
     static ArrayList<String> subjects = new ArrayList<>();
 
     static ArrayList<Teacher> teachers = new ArrayList<>();
@@ -39,7 +39,8 @@ public class Utilities implements Serializable {
         JPanel panel2 = new JPanel();
         panel1.add(new JLabel(text));
         JButton button = new JButton("OK");
-        panel2.add(closeButton(frame, null));
+        if (listener != null)
+            panel2.add(closeButton(frame, null));
         panel2.add(button);
         button.addActionListener(listener == null ? e -> frame.dispose() : listener);
         button.addActionListener(e -> frame.dispose());
@@ -78,9 +79,12 @@ public class Utilities implements Serializable {
             if (!new File(path).exists())
                 new File(path).createNewFile();
             ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(path));
-            ArrayList<ArrayList<? extends Main>> objects = new ArrayList<>();
+            ArrayList<ArrayList> objects = new ArrayList<>();
+            ArrayList<HashMap<String, LocalTime>> mapList = new ArrayList<HashMap<String, LocalTime>>() {{add(periods);}};
             objects.add(teachers);
             objects.add(classes);
+            objects.add(subjects);
+            objects.add(mapList);
             out.writeObject(objects);
             out.close();
         } catch (IOException e) {
@@ -92,9 +96,11 @@ public class Utilities implements Serializable {
     static void load() {
         if (new File(path).exists()) try {
             ObjectInputStream in = new ObjectInputStream(new FileInputStream(path));
-            ArrayList<ArrayList<? extends Main>> objects = ((ArrayList<ArrayList<? extends Main>>) in.readObject());
+            ArrayList<ArrayList> objects = ((ArrayList<ArrayList>) in.readObject());
             teachers = (ArrayList<Teacher>) objects.get(0);
             classes = ((ArrayList<Grade>) objects.get(1));
+            subjects = ((ArrayList<String>)objects.get(2));
+            periods = ((ArrayList<HashMap<String, LocalTime>>)objects.get(3)).get(0);
             in.close();
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
@@ -129,6 +135,27 @@ public class Utilities implements Serializable {
     }
 
     static void savePeriods(JPanel panel) {
+        HashMap<String, LocalTime> map = new HashMap<>();
+        boolean error = false;
+        for (Component e : panel.getComponents()) {
+            Component[] c = ((JPanel) e).getComponents();
+            String text = ((JLabel) c[0]).getText();
+            text = text.charAt(0) == '1' && text.charAt(1) != '.' ? text.substring(0, 2) : text.substring(0, 1);
+            LocalTime begin = LocalTime.parse(((JComboBox<String>) c[1]).getSelectedItem() + ":" + ((JComboBox<String>) c[3]).getSelectedItem());
+            LocalTime end = LocalTime.parse(((JComboBox<String>) c[5]).getSelectedItem() + ":" + ((JComboBox<String>) c[7]).getSelectedItem());
+            if (begin.isAfter(end)) {
+                alert("Der Stundenanfang kann nicht nach Stundenende liegen.", null, null);
+                error = true;
+            }
+            map.put(text + "begin", begin);
+            map.put(text + "end", end);
+        }
+        if (!error) periods = map;
+    }
 
+    static String formatTime(Integer time) {
+        if (Integer.toString(time).length() == 1)
+            return "0" + time;
+        return Integer.toString(time);
     }
 }
